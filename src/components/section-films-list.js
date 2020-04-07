e0ff3544a028d340142ddc63288e77d9d20649bd
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-04-08 00:26:20 +0300

    настраивает сборку, разбивает на модули

---
+++ src/components/section-films-list.js
@@ -0,0 +1,7 @@
+export const createSectionFilmsList = () => {
+  return (
+    `<section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      </section>`
+  );
+};
