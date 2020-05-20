859522f83703cb37d90189df679c232d76fc4f5b
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-05-20 23:11:34 +0300

    наследование от абстрактного класса в классе userrank

--- src/components/user-rank.js
+++ src/components/user-rank.js
@@ -1,4 +1,4 @@
-import {createElement} from "../utils";
+import AbstractComponent from "./abstract-component";
 
 export const createUserRank = () => {
   return (
@@ -9,24 +9,8 @@ export const createUserRank = () => {
   );
 };
 
-export default class UserRank {
-  constructor() {
-    this._element = null;
-  }
-
+export default class UserRank extends AbstractComponent {
   getTemplate() {
     return createUserRank();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
