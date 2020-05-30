08b039b8e97aa3efc07ba1cdaf8408005934dba7
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-05-30 22:57:25 +0300

    изменяет принцип определения типа фильтрации

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,20 +1,20 @@
 import AbstractComponent from "./abstract-component";
 
 export const FilterType = {
-  ALL: `#all`,
-  WATCHLIST: `#watchlist`,
-  HISTORY: `#history`,
-  FAVORITES: `#favorites`
+  ALL: `all`,
+  WATCHLIST: `watchlist`,
+  HISTORY: `history`,
+  FAVORITES: `favorites`
 };
 
 const createNavigation = (watchlist, history, favorites) => {
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
-        <a href="${FilterType.ALL}" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="${FilterType.WATCHLIST}" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
-        <a href="${FilterType.HISTORY}" class="main-navigation__item">History <span class="main-navigation__item-count">${history}</span></a>
-        <a href="${FilterType.FAVORITES}" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
+        <a href="#all" data-filter-type ="${FilterType.ALL}" class="main-navigation__item main-navigation__item--active">All movies</a>
+        <a href="#watchlist" data-filter-type ="${FilterType.WATCHLIST}" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
+        <a href="#history" data-filter-type ="${FilterType.HISTORY}" class="main-navigation__item">History <span class="main-navigation__item-count">${history}</span></a>
+        <a href="#favorites" data-filter-type ="${FilterType.FAVORITES}" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`
@@ -48,7 +48,7 @@ export default class Navigation extends AbstractComponent {
         return;
       }
 
-      const filterType = evt.target.attributes.href.textContent;
+      const filterType = evt.target.dataset.filterType;
 
       if (this._currenFilterType === filterType) {
         return;
